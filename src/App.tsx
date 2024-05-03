d98a9eb68c6d309752965b2a1e3b712452589b7e
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-03 20:09:04 -0300

    Primeros pasos con código CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@
 const App = () => {
 
   return (
-    <div>App</div>
+    <>
+      <div className="container">
+        <h1 className="app-title">Cotizador de <span>Criptomonedas</span></h1>
+        <div className="content">
+          1
+        </div>
+      </div>
+    </>
   )
 }
 
