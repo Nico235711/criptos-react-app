d00ac8e07f48c784eab0ed873877e6424d6dde44
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-05-04 19:03:15 -0300

    Schema de Zod y Mostrando las Monedas

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,4 @@
+import { currencies } from "../data"
 
 const Form = () => {
 
@@ -6,7 +7,10 @@ const Form = () => {
       <div className="field">
         <label htmlFor="currency">Moneda:</label>
         <select name="currency" id="currency">
-          <option value="">-- Seleccione --</option>
+          <option value="">-- Seleccione una Moneda --</option>
+          {currencies.map(currency => (
+            <option value={currency.code} key={currency.code}>{currency.name}</option>
+          ))}
         </select>
       </div>
 
